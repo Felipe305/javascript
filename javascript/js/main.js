f647f8f322d700fe9b2062dd563b50014fdd05dc
Felipe Silva <felipe.profissionalti@gmail.com>
2021-08-16 22:17:52 -0300

    Add files via upload

---
+++ javascript/js/main.js
@@ -0,0 +1,71 @@
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+
+/*
+var d = new Date();
+alert(d.getday());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade")
+};*/
+    
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+
+/*var fruta = { nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" | "));
+
+
+//var nome = "Felipe Silva";
+//var idade = 18;
+//var idade2 = 10;
+//var frase = "Brasil é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toUpperCase());
